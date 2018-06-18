6969339c617bdb934ca2fc4734da266e9167590d
VIKESH <kvikesh800@gmail.com>
2018-06-18 21:33:25 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -16,7 +16,8 @@ makeCacheMatrix <- function( mat = matrix() ) {
 }
 
 
-## calculate inverse of the  matrix returned by "makeCacheMatrix"
+## calculate inverse of the  matrix returned by "makeCacheMatrix" 
+## and if inverse is calculated previously then it returns the cached inverse
 cacheSolve <- function(x, ...) {
         mat <- x$getinverse()
         if( !is.null(mat) ) {
@@ -28,3 +29,14 @@ cacheSolve <- function(x, ...) {
         x$setinverse(mat)
         mat
 }
+
+
+
+## Steps to reproduce the solution
+## Let the matrix is a 3X3 square matrix with elements as 
+##  [1, 2, 3]
+##  [4, 4, 6]
+##  [7, 0, 9]
+## mat<-matrix(c(1,2,3,4,4,6,7,0,9),3,3)
+## m1 <- makeCacheMatrix(mat)
+## cacheSolve(m1)
